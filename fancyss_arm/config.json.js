299fd222fe5dcaa55ebc4464a1a81f782b5f3ac9
koolshe <51077579+koolshe@users.noreply.github.com>
2019-05-27 18:50:34 +0800

    fancyss
    fancyss是一个项目，在基于asuswrt / merlin的路由器上为GFW提供工具。

---
+++ fancyss_arm/config.json.js
@@ -0,0 +1,10 @@
+{
+"build_date":"2018-11-25_22:15:09",
+"description":"科学上网",
+"home_url":"Main_Ss_Content.asp",
+"md5":"75a1791f7182580b753d417a201da534",
+"name":"shadowsocks",
+"tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm/shadowsocks.tar.gz", 
+"title":"科学上网",
+"version":"4.1.7"
+}
---
+++ fancyss_hnd/config.json.js
@@ -0,0 +1,10 @@
+{
+"build_date":"2019-05-21_21:55:57",
+"description":"科学上网",
+"home_url":"Module_shadowsocks.asp",
+"md5":"8d52c77c01087713bf3f40de4ebfacb9",
+"name":"shadowsocks",
+"tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
+"title":"科学上网",
+"version":"1.6.6"
+}
